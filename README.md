9d4c0bac3c24540d50322259992eea6e447c5d39
WuYujie <coffee377@dingtalk.com>
2021-02-23 09:45:45 +0800

    :zap: 子模块仓库地址修改

--- README.md
+++ README.md
@@ -7,11 +7,16 @@
     git submodule update --init --recursive
 
     git clone --recurse-submodules <main_project_url>  # 获取主项目和所有子项目源码
-    
 
 ### 删除 submodule
+
 1. 删除项目源码子目录
 2. 删除 .gitmodules 文件中 submodule 的信息
 3. 删除 .git/config 文件中 submodule 的信息
 4. 删除 .git/modules/ 目录下 sumodule 的子目录
 5. 应用修改
+
+## TODO
+
+- [ ] 迁移插件D:\Project\mydev\finesoft\plugins
+
--- plugins/plugin-login/src/main/java/com/voc/fr/plugin/login/VisualLoginPage.java
+++ plugins/plugin-login/src/main/java/com/voc/fr/plugin/login/VisualLoginPage.java
@@ -5,21 +5,32 @@ import com.fr.decision.web.LoginComponent;
 import com.fr.general.Inter;
 import com.fr.intelli.record.Focus;
 import com.fr.intelli.record.Original;
-import com.fr.plugin.transform.FunctionRecorder;
 import com.fr.record.analyzer.EnableMetrics;
 import com.fr.web.struct.Atom;
+import com.voc.fr.tool.annotation.core.FunctionDefineProvider;
 import com.voc.fr.tool.annotation.decision.LoginPageProvider;
+import com.voc.fr.tool.annotation.design.PresentKindProvider;
+import com.voc.fr.tool.annotation.design.ServerTableDataDefineProvider;
+import com.voc.fr.tool.annotation.design.TableDataDefineProvider;
 import com.voc.fr.tool.annotation.plugin.ChangeNote;
+import com.voc.fr.tool.annotation.plugin.ChangeNotes;
 
 /**
  * @author Wu Yujie
  * @email coffee377@dingtalk.com
  * @time 2020/04/20 11:51
  */
-@FunctionRecorder
 @EnableMetrics
-@ChangeNote(dateOf = "2020-04-21", content = {"插件初始化"})
+@ChangeNote(dateOf = "2020-04-21", contents = {"插件初始化", "测试信息"})
 @LoginPageProvider
+@ChangeNotes({
+        @ChangeNote(dateOf = "2020-04-22", contents = {"测试1"}),
+        @ChangeNote(dateOf = "2019-04-26", contents = {"测试2"})
+})
+@FunctionDefineProvider(name = "Test")
+@PresentKindProvider
+@TableDataDefineProvider
+@ServerTableDataDefineProvider
 public class VisualLoginPage extends AbstractLoginPageProvider {
 
     @Override
